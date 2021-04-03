607d75e4470d66240656aab29b054d77a3627cd6
database64128 <free122448@hotmail.com>
2021-04-03 20:24:36 +0800

    ⛑️ Rescue tool: reconstruct config from generated online config

---
+++ ShadowsocksUriGenerator.Rescue.CLI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShadowsocksUriGenerator.Rescue.CLI
+{
+    internal class Program
+    {
+        private static Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand("A rescue tool CLI for restoring ss-uri-gen config from generated online config directory.")
+            {
+                new Option<string>("--online-config-dir", "Directory of generated online config."),
+                new Option<string>("--output-dir", "Output directory."),
+            };
+            rootCommand.AddValidator(ValidateRootCommand);
+            rootCommand.Handler = CommandHandler.Create<string, string, CancellationToken>(HandleRootCommand);
+
+            Console.OutputEncoding = Encoding.UTF8;
+            return rootCommand.InvokeAsync(args);
+        }
+
+        private static string? ValidateRootCommand(CommandResult commandResult)
+        {
+            var hasOnlineConfigDir = commandResult.Children.Contains("--online-config-dir");
+
+            if (!hasOnlineConfigDir)
+                return "Specify path to online config directory with `--online-config-dir`.";
+            else
+                return null;
+        }
+
+        private static async Task<int> HandleRootCommand(string onlineConfigDir, string outputDir, CancellationToken cancellationToken = default)
+        {
+            var (users, nodes, errMsgGetFromOC) = await Rescuers.FromOnlineConfig(onlineConfigDir, cancellationToken);
+            if (errMsgGetFromOC is not null || users is null || nodes is null)
+            {
+                Console.WriteLine(errMsgGetFromOC);
+                return -1;
+            }
+
+            var errMsgSaveJson = await Restorers.ToJsonFiles(outputDir, users, nodes, cancellationToken);
+            if (errMsgSaveJson is not null)
+            {
+                Console.WriteLine(errMsgSaveJson);
+                return -2;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ShadowsocksUriGenerator.Rescue/Restorers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShadowsocksUriGenerator.Rescue
+{
+    /// <summary>
+    /// Class for static methods that
+    /// restore order from information collected by rescuers.
+    /// </summary>
+    public static class Restorers
+    {
+        /// <summary>
+        /// Restores rescued <paramref name="users"/> and <paramref name="nodes"/>
+        /// to JSON config files.
+        /// </summary>
+        /// <param name="configDir">Path to JSON config directory.</param>
+        /// <param name="users">The rescued <see cref="Users"/> object.</param>
+        /// <param name="nodes">The rescued <see cref="Nodes"/> object.</param>
+        /// <param name="cancellationToken">A token that may be used to cancel the write operation.</param>
+        /// <returns>An error message. Null if no errors occurred.</returns>
+        public static async Task<string?> ToJsonFiles(string configDir, Users users, Nodes nodes, CancellationToken cancellationToken = default)
+        {
+            // Make sure configDir is either empty or a path that ends with a slash.
+            if (!string.IsNullOrEmpty(configDir) && !(configDir.EndsWith('/') || configDir.EndsWith('\\')))
+                configDir = $"{configDir}/";
+
+            var usersErrMsg = await Utilities.SaveJsonAsync($"{configDir}Users.json",
+                                                            users,
+                                                            Utilities.commonJsonSerializerOptions,
+                                                            false,
+                                                            false,
+                                                            cancellationToken);
+            var nodesErrMsg = await Utilities.SaveJsonAsync($"{configDir}Nodes.json",
+                                                            nodes,
+                                                            Utilities.commonJsonSerializerOptions,
+                                                            false,
+                                                            false,
+                                                            cancellationToken);
+
+            if (usersErrMsg is not null && nodesErrMsg is not null)
+                return $"{usersErrMsg}{Environment.NewLine}{nodesErrMsg}";
+            else if (usersErrMsg is not null)
+                return usersErrMsg;
+            else if (nodesErrMsg is not null)
+                return nodesErrMsg;
+            else
+                return null;
+        }
+    }
+}
