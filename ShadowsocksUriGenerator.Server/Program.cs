1c820d058480e1514b57b4a859a0a578aa8d5558
database64128 <free122448@hotmail.com>
2022-07-02 15:08:22 +0800

    🦉 Migrate to new hosting model

--- ShadowsocksUriGenerator.Server/Program.cs
+++ ShadowsocksUriGenerator.Server/Program.cs
@@ -1,52 +1,79 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using ShadowsocksUriGenerator.CLI.Utils;
-using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Hosting;
-using System.CommandLine.Parsing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using ShadowsocksUriGenerator;
+using ShadowsocksUriGenerator.Services;
+using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
 
-namespace ShadowsocksUriGenerator.Server
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
-    internal class Program
+    Trace.Assert(config.Sources.Count == 4);
+
+    config.Sources.RemoveAt(3);
+    config.Sources.RemoveAt(2);
+    config.Sources.RemoveAt(1);
+
+    config.SetBasePath(FileHelper.configDirectory)
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+    config.AddEnvironmentVariables();
+});
+
+builder.Services.AddSingleton<IDataService, DataService>();
+builder.Services.AddHostedService(provider => provider.GetService<IDataService>() as DataService ?? throw new Exception("Injected IDataService is not DataService."));
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+                    options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+                    options.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+                });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
-        private static Task Main(string[] args)
-        {
-            var cancellationTokenBinder = new CancellationTokenBinder();
-            var rootCommand = new RootCommand("Shadowsocks URI Generator API Server provides an API endpoint for basic management tasks and online config.");
+        Title = "Shadowsocks URI Generator API Server",
+        Description = "Shadowsocks URI Generator API Specifications",
+        Version = "v1",
+    });
 
-            rootCommand.SetHandler(cancellationToken => cancellationToken.WaitHandle.WaitOne(), cancellationTokenBinder);
+    var xmlPath = $"{AppContext.BaseDirectory}{Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0])}.xml";
 
-            var parser = new CommandLineBuilder(rootCommand)
-                .UseHost(_ => Host.CreateDefaultBuilder()
-                    .ConfigureAppConfiguration((hostingContext, config) =>
-                    {
-                        Trace.Assert(config.Sources.Count == 4);
-
-                        config.Sources.RemoveAt(3);
-                        config.Sources.RemoveAt(2);
-                        config.Sources.RemoveAt(1);
-
-                        config.SetBasePath(FileHelper.configDirectory)
-                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                              .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
-                        config.AddEnvironmentVariables();
-                    }),
-                    host =>
-                    {
-                        host.ConfigureWebHostDefaults(webBuilder =>
-                        {
-                            webBuilder.UseStartup<Startup>();
-                        });
-                    })
-                .UseDefaults()
-                .Build();
-
-            return parser.InvokeAsync(args);
-        }
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
     }
-}
+});
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.All;
+    options.ForwardLimit = null;
+    options.KnownProxies.Clear();
+    options.KnownNetworks.Clear();
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseReDoc();
+
+app.UseForwardedHeaders();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
